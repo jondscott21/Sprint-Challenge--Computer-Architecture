faa5aeed88b85106db968a06278f51913da32877
Jonathan Scott <jonathan.scott@gmail.com>
2020-01-31 09:04:37 -0800

    mvp hit

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,22 +3,22 @@
 import sys
 import datetime
 
-LDI = 0b10000010
-PRN = 0b01000111
-MUL = 0b10100010
-ADD = 0b10100000
-SUB = 0b10100001
-DIV = 0b10100011
-HLT = 0b00000001
-PUSH = 0b01000101
-POP = 0b01000110
-CALL = 0b01010000
-RET = 0b00010001
+LDI = 0b10000010 #130
+PRN = 0b01000111 #71
+MUL = 0b10100010 #162
+ADD = 0b10100000 #160
+SUB = 0b10100001 #161
+DIV = 0b10100011 #163
+HLT = 0b00000001 #1
+PUSH = 0b01000101 #69
+POP = 0b01000110 #70
+CALL = 0b01010000 #80
+RET = 0b00010001 #17
 
-CMP = 0b10100111
-JMP = 0b01010100
-JEQ = 0b01010101
-JNE = 0b01010110
+CMP = 0b10100111 #167
+JMP = 0b01010100 #84
+JEQ = 0b01010101 #85
+JNE = 0b01010110 #86
 class CPU:
     """Main CPU class."""
 
@@ -32,7 +32,10 @@ class CPU:
         self.pc = 0
         self.sp = 7
         self.IS = 6
-        self.FL = 0
+        self.FL = [0] * 8
+        self.less = 0
+        self.greater = 1
+        self.equal = 2
         self.reg[self.sp] = 0xF4
         self.branchtable = {}
         self.branchtable[LDI] = self.handle_LDI
@@ -161,7 +164,6 @@ class CPU:
         self.pc += 2
     
     def handle_CALL(self):
-        # print(self.ram_read(self.pc))
         val = self.pc + 2
         self.reg[self.sp] -= 1
 
@@ -177,33 +179,50 @@ class CPU:
         self.pc = self.ram_read(return_address)
         self.reg[self.sp] += 1
 
-    def handle_CMP():
+    def handle_CMP(self):
         # Compare the values in two registers.
-
+        op_a = self.ram_read(self.pc + 1)
+        op_b = self.ram_read(self.pc + 2)
         # * If they are equal, set the Equal `E` flag to 1, otherwise set it to 0.
-
+        if self.reg[op_a] == self.reg[op_b]:
+            self.FL[self.equal] = 1
+        else:
+            self.FL[self.equal] = 0
         # * If registerA is less than registerB, set the Less-than `L` flag to 1,
         # otherwise set it to 0.
-
+        if self.reg[op_a] < self.reg[op_b]:
+            self.FL[self.less] = 1
+        else:
+            self.FL[self.less] = 0
         # * If registerA is greater than registerB, set the Greater-than `G` flag
         # to 1, otherwise set it to 0.
-        pass
+        if self.reg[op_a] > self.reg[op_b]:
+            self.FL[self.greater] = 1
+        else:
+            self.FL[self.greater] = 0
+        self.pc += 3
 
-    def handle_JMP():
+    def handle_JMP(self):
         # Jump to the address stored in the given register.
-
+        jump = self.ram_read(self.pc + 1)
+        # print(jump)
         # Set the `PC` to the address stored in the given register.
-        pass
-
-    def handle_JEQ():
+        self.pc = self.reg[jump]
+    def handle_JEQ(self):
         # If `equal` flag is set (true),
         #  jump to the address stored in the given register.
-        pass
+        if self.FL[self.equal] == 1:
+            self.pc = self.reg[self.ram_read(self.pc + 1)]
+        else:
+            self.pc += 2
 
-    def handle_JNE():
+    def handle_JNE(self):
         # If `E` flag is clear (false, 0), 
         # jump to the address stored in the given register.
-        pass
+        if self.FL[self.equal] == 0:
+            self.pc = self.reg[self.ram_read(self.pc + 1)]
+        else:
+            self.pc += 2
 
     def run(self):
         """Run the CPU."""
@@ -213,5 +232,4 @@ class CPU:
             # if datetime.datetime.now() - t < datetime.timedelta(seconds=1):
             #     self.reg[self.IS] = 1
             IR = self.ram[self.pc]
-            print("IR: ", IR)
             self.branchtable[IR]()
